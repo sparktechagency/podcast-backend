40e55cbb64dce088acc18773ff23b30573952d71
manik <maniksarker652@gmail.com>
2025-07-10 10:47:38 +0600

    create apis for sub category

--- src/app/modules/subCategory/subCategory.routes.ts
+++ src/app/modules/subCategory/subCategory.routes.ts
@@ -1,25 +1,50 @@
-import express from "express";
-import auth from "../../middlewares/auth";
-import { USER_ROLE } from "../user/user.constant";
-import validateRequest from "../../middlewares/validateRequest";
-import subCategoryValidations from "./subCategory.validation";
-import subCategoryController from "./subCategory.controller";
-import { uploadFile } from "../../helper/fileUploader";
+import express, { Request, Response, NextFunction } from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import validateRequest from '../../middlewares/validateRequest';
+import subCategoryValidation from './subCategory.validation';
+import subCategoryController from './subCategory.controller';
+import { uploadFile } from '../../helper/mutler-s3-uploader';
 
 const router = express.Router();
 
-router.patch(
-    "/update-profile",
-    auth(USER_ROLE.user),
+router.post(
+    '/create-subcategory',
+    auth(USER_ROLE.superAdmin),
     uploadFile(),
-    (req, res, next) => {
+    (req: Request, res: Response, next: NextFunction) => {
         if (req.body.data) {
             req.body = JSON.parse(req.body.data);
         }
         next();
     },
-    validateRequest(subCategoryValidations.updateSubCategoryData),
-    subCategoryController.updateUserProfile
+    validateRequest(subCategoryValidation.createSubCategoryValidationSchema),
+    subCategoryController.createSubCategory
+);
+
+router.patch(
+    '/update-subcategory/:id',
+    auth(USER_ROLE.superAdmin),
+    uploadFile(),
+    (req: Request, res: Response, next: NextFunction) => {
+        if (req.body.data) {
+            req.body = JSON.parse(req.body.data);
+        }
+        next();
+    },
+    validateRequest(subCategoryValidation.updateSubCategoryValidationSchema),
+    subCategoryController.updateSubCategory
+);
+
+router.get('/all-subcategories', subCategoryController.getAllSubCategories);
+router.get(
+    '/get-single-subcategory/:id',
+    subCategoryController.getSingleSubCategory
+);
+router.delete(
+    '/delete-subcategory/:id',
+    auth(USER_ROLE.superAdmin),
+    subCategoryController.deleteSubCategory
 );
 
 export const subCategoryRoutes = router;
