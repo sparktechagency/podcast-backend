9c5371b969cf5f35e19121110c61f2121bdb1f18
manik <maniksarker652@gmail.com>
2025-07-12 09:13:31 +0600

    route setup for album

--- src/app/modules/album/album.routes.ts
+++ src/app/modules/album/album.routes.ts
@@ -1,25 +1,57 @@
-import express from "express";
-import auth from "../../middlewares/auth";
-import { USER_ROLE } from "../user/user.constant";
-import validateRequest from "../../middlewares/validateRequest";
-import albumValidations from "./album.validation";
-import albumController from "./album.controller";
-import { uploadFile } from "../../helper/fileUploader";
+import express, { Request, Response, NextFunction } from 'express';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { uploadFile } from '../../helper/mutler-s3-uploader';
+import AlbumValidations from './album.validation';
+import AlbumController from './album.controller';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
-router.patch(
-    "/update-profile",
+router.post(
+    '/create',
     auth(USER_ROLE.user),
     uploadFile(),
-    (req, res, next) => {
+    (req: Request, res: Response, next: NextFunction) => {
         if (req.body.data) {
             req.body = JSON.parse(req.body.data);
         }
         next();
     },
-    validateRequest(albumValidations.updateAlbumData),
-    albumController.updateUserProfile
+    validateRequest(AlbumValidations.createAlbumValidationSchema),
+    AlbumController.createAlbum
+);
+
+router.get(
+    '/all-albums',
+    auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.user),
+    AlbumController.getAllAlbums
+);
+
+router.get(
+    '/single-album/:albumId',
+    auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.user),
+    AlbumController.getAlbumById
+);
+
+router.patch(
+    '/update/:albumId',
+    auth(USER_ROLE.user),
+    uploadFile(),
+    (req: Request, res: Response, next: NextFunction) => {
+        if (req.body.data) {
+            req.body = JSON.parse(req.body.data);
+        }
+        next();
+    },
+    validateRequest(AlbumValidations.updateAlbumValidationSchema),
+    AlbumController.updateAlbum
+);
+
+router.delete(
+    '/delete/:albumId',
+    auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.user),
+    AlbumController.deleteAlbum
 );
 
 export const albumRoutes = router;
