4085be7ac5baf0756063b69172c948b609fd0002
manik <maniksarker652@gmail.com>
2025-07-13 11:39:48 +0600

    make some changes

--- src/app/modules/manage-web/manage.model.ts
+++ src/app/modules/manage-web/manage.model.ts
@@ -2,126 +2,126 @@ import mongoose, { model } from 'mongoose';
 
 //! Privacy and policy
 const privacySchema = new mongoose.Schema(
-  {
-    description: {
-      type: String,
-      required: true,
+    {
+        description: {
+            type: String,
+            required: true,
+        },
     },
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      virtuals: true,
-    },
-  },
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+        },
+    }
 );
-//! About US
+//! About US----
 const aboutUsSchema = new mongoose.Schema(
-  {
-    description: {
-      type: String,
-      required: true,
+    {
+        description: {
+            type: String,
+            required: true,
+        },
     },
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      virtuals: true,
-    },
-  },
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+        },
+    }
 );
 //! Terms Conditions
 const termsAndConditionsSchema = new mongoose.Schema(
-  {
-    description: {
-      type: String,
-      required: true,
+    {
+        description: {
+            type: String,
+            required: true,
+        },
     },
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      virtuals: true,
-    },
-  },
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+        },
+    }
 );
 const partnerSchema = new mongoose.Schema(
-  {
-    description: {
-      type: String,
-      required: true,
+    {
+        description: {
+            type: String,
+            required: true,
+        },
     },
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      virtuals: true,
-    },
-  },
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+        },
+    }
 );
 //!Contact US
 const contactUsSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
+    {
+        email: {
+            type: String,
+            required: true,
+        },
+        phone_number: {
+            type: String,
+            required: true,
+        },
     },
-    phone_number: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      virtuals: true,
-    },
-  },
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+        },
+    }
 );
 //!FAQ
 const faqSchema = new mongoose.Schema(
-  {
-    question: {
-      type: String,
-      required: true,
+    {
+        question: {
+            type: String,
+            required: true,
+        },
+        answer: {
+            type: String,
+            required: true,
+        },
     },
-    answer: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      virtuals: true,
-    },
-  },
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+        },
+    }
 );
 //!Slider
 const sliderSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
     },
-    image: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      virtuals: true,
-    },
-  },
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+        },
+    }
 );
 
 export const PrivacyPolicy = model('PrivacyPolicy', privacySchema);
 export const AboutUs = model('AboutUs', aboutUsSchema);
 export const TermsConditions = model(
-  'TermsConditions',
-  termsAndConditionsSchema,
+    'TermsConditions',
+    termsAndConditionsSchema
 );
 export const ContactUs = model('ContactUs', contactUsSchema);
 export const FAQ = model('FAQ', faqSchema);
