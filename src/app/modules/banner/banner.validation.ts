ab03b668308e1820d9ad95f63f491e2c377ddbc1
manik <maniksarker652@gmail.com>
2025-08-19 10:19:48 +0600

    create validation schema for banner

--- src/app/modules/banner/banner.validation.ts
+++ src/app/modules/banner/banner.validation.ts
@@ -1,9 +1,30 @@
 import { z } from 'zod';
 
 export const createBannerValidationSchema = z.object({
-    body: z.object({
-        image: z.string({ required_error: 'Image is required' }).optional(),
-    }),
+    body: z
+        .object({
+            image: z.string().min(1),
+            url: z.string().url().optional(),
+            startDate: z
+                .date()
+                .refine(
+                    (date) => date instanceof Date && !isNaN(date.getTime())
+                ),
+            endDate: z
+                .date()
+                .refine(
+                    (date) => date instanceof Date && !isNaN(date.getTime())
+                ),
+        })
+        .superRefine((data, ctx) => {
+            if (data.endDate <= data.startDate) {
+                ctx.addIssue({
+                    path: ['endDate'],
+                    message: 'End date must be later than start date',
+                    code: z.ZodIssueCode.custom,
+                });
+            }
+        }),
 });
 
 const BannerValidations = { createBannerValidationSchema };
