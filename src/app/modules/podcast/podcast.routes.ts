54d3f0cd4c61cc626fc11421fd0e6a930ebc1376
manik <maniksarker652@gmail.com>
2025-09-15 18:31:07 +0600

    s

--- src/app/modules/podcast/podcast.routes.ts
+++ src/app/modules/podcast/podcast.routes.ts
@@ -1,14 +1,14 @@
-import auth from '../../middlewares/auth';
-import { USER_ROLE } from '../user/user.constant';
-import validateRequest from '../../middlewares/validateRequest';
-import podcastValidation from './podcast.validation';
-import podcastController from './podcast.controller';
 import express, { NextFunction, Request, Response } from 'express';
 import { uploadFile } from '../../helper/mutler-s3-uploader';
+import auth from '../../middlewares/auth';
 import { publicCache } from '../../middlewares/cacheControl';
+import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from '../user/user.constant';
+import podcastController from './podcast.controller';
+import podcastValidation from './podcast.validation';
 
 const router = express.Router();
-
+//
 router.post(
     '/create',
     auth(USER_ROLE.creator),
