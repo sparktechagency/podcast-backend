6223e271a2e03276c6d61c35ff3c55f07e3b263b
manik <maniksarker652@gmail.com>
2025-09-16 10:21:48 +0600

    s

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,8 @@ const app: Application = express();
 dotenv.config();
 
 // parser---------
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(
     cors({
